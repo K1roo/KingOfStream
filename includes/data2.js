e01ef019e130caad211d9db0ae557606daa011c3
K1roo <96072937+K1roo@users.noreply.github.com>
2023-02-09 11:46:16 +0100

    feat(KoSapp): app v1.1

---
+++ includes/data2.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers aka our JSON file
+
+let questions = [
+    {
+    numb: 1,
+    question: "test1 ",
+    answer: "Garen",
+    options: [
+      "Garen",
+      "Sova",
+      "Morgana",
+      "Lxdykilla"
+    ]
+  },
+    {
+    numb: 2,
+    question: " test2",
+    answer: "Worstmissfor",
+    options: [
+      "wAlrustyFrosty",
+      "Jaxxson.-",
+      "Worstmissfor",
+      "CassandraouTTV"
+    ]
+  },
+    {
+    numb: 3,
+    question: "test3 ",
+    answer: "3",
+    options: [
+      "0",
+      "1",
+      "2",
+      "3"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does LCS stand for?",
+    answer: "League Championship Series",
+    options: [
+      "League Correlation Statistics",
+      "League Computationnal Structure",
+      "League Champions Summer",
+      "League Championship Series"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Summoner name was 'illiasendetskyi' ?  ",
+    answer: "Kyphoo",
+    options: [
+      "Darius",
+      "Kyphoo",
+      "Sett",
+      "JaX"
+    ]
+  },
+];
